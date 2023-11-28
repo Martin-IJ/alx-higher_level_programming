a527b2b2bd46e026016ef7e7415d4caddb2ba187
Martins Ohez <martinohez@gmail.com>
2023-11-27 16:21:46 -0800

    added 5-10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * check_cycle - Entry Point
+ * Description: Check if signly linked list has cycle in it
+ * @list: pointer to head of list
+ * Return: 1 for true, 0 for false
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
+
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
+}
---
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+print("The Zen of Python, by Tim Peters\n\n" +
+      ".\n".join("import this".split("\n")[2:]))
